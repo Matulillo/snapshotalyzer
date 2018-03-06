79a70253addfe120df76a72a4349bac46bb49585
unknown <crodriguezhervas@gmail.com>
2018-03-06 18:33:43 +0100

    Added more resilience funtionality for snapshot creation

--- matulillo/matulillo.py
+++ matulillo/matulillo.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='matulillo')
@@ -42,9 +43,6 @@ def list_snapshots(project):
                     s.start_time.strftime("%c")
                 )))
     return
-
-
-
 
 
 @cli.group('volumes')
@@ -132,7 +130,12 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Cloud not stop {0}. ".format(i.id) + str(e))
+            continue
+
     return
 
 
@@ -146,7 +149,12 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Cloud not start {0}. ".format(i.id) + str(e))
+            continue
+
     return
 
 
